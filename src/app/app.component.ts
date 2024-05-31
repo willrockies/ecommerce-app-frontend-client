284797f1798a4628a8852b271d6e57b0ff565d7b
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-05-31 15:42:35 -0300

    Creating a products interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  products: any[] = [];
+  products: Product[] = [];
   constructor(private http: HttpClient) {
 
   }
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe(
       {
-        next: (response: any) => this.products = response.data, //console.log(response), // what to do next
+        next: (response) => this.products = response.data, //console.log(response), // what to do next
         error: error => console.log(error),
         complete: () => {
           console.log('requested complete');
