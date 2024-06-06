bb29fece4a87d3d91dc027865d59fd385f5c73f5
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-06-06 15:26:48 -0300

    intro to angular service

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Pagination<Product[]>> (this.baseUrl + 'products');
+  }
 }
