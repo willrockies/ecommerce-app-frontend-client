ee5d48a8c8f778e94c4ea024769ff459c5d9cf2e
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-29 16:33:46 -0300

    Handling validation errrors

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -19,8 +19,13 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error) {
-          if(error.status === 400) {
-            this.toastr.error(error.error.message, error.status.toString());
+          if (error.status === 400) {
+            if (error.error.errors) {
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.status.toString());
+            }
+
           }
 
           if (error.status === 401) {
