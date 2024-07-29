4d050b7f107ade1359596e2102ab88e1e033e676
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-29 15:45:27 -0300

    Creating additional error components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
@@ -10,6 +12,8 @@ import { TestErrorComponent } from './test-error/test-error.component';
   declarations: [
     NavBarComponent,
     TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     CommonModule,
