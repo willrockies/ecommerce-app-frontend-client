11a758f0859da27f13aae9e397acef26449aa643
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-02 14:28:06 -0300

    Basket service methods

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Basket } from '../shared/models/basket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<Basket | null>(null);
+  basketSource$ = this.basketSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string) {
+    return this.http.get<Basket>(this.baseUrl + 'basket?id=' + id).subscribe({
+      next: (response) => this.basketSource.next(response),
+      error: (error) => console.log(error)
+
+    });
+  }
+
+  setBasket(basket: Basket) {
+    return this.http.post<Basket>(this.baseUrl + 'basket', basket).subscribe({
+      next: basket => this.basketSource.next(basket)
+    })
+  }
+
+  getCurrentBasketValue() {
+    return this.basketSource.value;
+  }
 }
