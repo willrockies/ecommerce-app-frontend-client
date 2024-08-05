6bcdadd34cb7cf11a819f5884641d1bbcdf1aa64
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-05 14:02:30 -0300

    Adding the basket totals to the service

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Basket, BasketItem } from '../shared/models/basket';
+import { Basket, BasketItem, BasketTotal } from '../shared/models/basket';
 import { Product } from '../shared/models/product';
 
 @Injectable({
@@ -12,12 +12,19 @@ export class BasketService {
   baseUrl = environment.apiUrl;
   private basketSource = new BehaviorSubject<Basket | null>(null);
   basketSource$ = this.basketSource.asObservable();
+  private basketTotalSource = new BehaviorSubject<BasketTotal | null>(null);
+  basketTotalSource$ = this.basketTotalSource.asObservable();
+
 
   constructor(private http: HttpClient) { }
 
   getBasket(id: string) {
     return this.http.get<Basket>(this.baseUrl + 'basket?id=' + id).subscribe({
-      next: (response) => this.basketSource.next(response),
+      next: (response) => {
+        this.basketSource.next(response);
+        this.calculateTotals();
+
+      },
       error: (error) => console.log(error)
 
     });
@@ -25,7 +32,10 @@ export class BasketService {
 
   setBasket(basket: Basket) {
     return this.http.post<Basket>(this.baseUrl + 'basket', basket).subscribe({
-      next: basket => this.basketSource.next(basket)
+      next: basket => {
+        this.basketSource.next(basket);
+        this.calculateTotals();
+      }
     })
   }
 
@@ -70,4 +80,14 @@ export class BasketService {
       type: item.productType
     }
   }
+
+  private calculateTotals() {
+    const basket = this.getCurrentBasketValue();
+    if (!basket) return;
+    const shipping = 0;
+    const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
+    const total = subtotal + shipping;
+    this.basketTotalSource.next({ shipping, subtotal, total });
+  }
+
 }
--- src/app/shared/models/basket.ts
+++ src/app/shared/models/basket.ts
@@ -20,3 +20,9 @@ export class Basket implements Basket {
   id = cuid();
   items: BasketItem[] = [];
 }
+
+export interface BasketTotal {
+  shipping: number;
+  subtotal: number;
+  total: number;
+}
