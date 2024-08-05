0ace97a3d7a3007ad7307c4a5e95020398655a71
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-05 16:05:01 -0300

    Adding the checkout module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'server-error', component: ServerErrorComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
+  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
---
+++ src/app/checkout/checkout-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './checkout.component';
+import { RouterModule } from '@angular/router';
+
+
+const routes = [
+  { path: '', component: CheckoutComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class CheckoutRoutingModule { }
